70132d2db96ccb44e399c9231bbd1b94bf1adcf3
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-25 23:04:27 +0000

    passing test for FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,9 @@
 public class FizzBuzz {
 
     public static String output(int a) {
-        if (a % 3 == 0) {
+        if (a % 15 == 0) {
+            return "FizzBuzz";
+        } else if (a % 3 == 0) {
             return "Fizz";
         } else if (a % 5 == 0) {
             return "Buzz";
